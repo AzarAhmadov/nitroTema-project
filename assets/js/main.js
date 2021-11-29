79fe4066e4aa0c5ea6c8ce659c6364d554eef259
azerahmedov <ehmedovh111@gmail.com>
2021-11-30 03:17:40 +0400

    duzenlemeler

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,34 @@
-var swiper = new Swiper(".mySwiper", {
+var swiper = new Swiper(".swiper-container", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+  }
+});
+;
+
+var swiper = new Swiper(".swip-two", {
     pagination: {
       el: ".swiper-pagination",
       type: "progressbar",
