72233c79635608b795758e90b39fcd4d8a7799b5
azerahmedov <ehmedovh111@gmail.com>
2021-11-30 18:22:17 +0400

    duzenlemeler

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,7 +8,7 @@ var swiper = new Swiper(".swiper-container", {
   slidesPerView: 1,
   spaceBetween: 10,
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
     clickable: true,
   },
   breakpoints: {
@@ -24,17 +24,46 @@ var swiper = new Swiper(".swiper-container", {
       slidesPerView: 3,
       spaceBetween: 20,
     },
-  }
+  },
 });
-;
 
 var swiper = new Swiper(".swip-two", {
-    pagination: {
-      el: ".swiper-pagination",
-      type: "progressbar",
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
+  pagination: {
+    el: ".swiper-pagination",
+    type: "progressbar",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+let mobilOpen = document.querySelector(".mobil-open");
+let openBar = document.querySelector(".open-menu");
+let closeBtn = document.querySelectorAll(".close-btn");
+let Body = document.querySelector("#body");
+
+openBar.addEventListener("click", () => {
+  mobilOpen.classList.add("active");
+  Body.classList.add("active");
+});
+
+for (let i = 0; i < closeBtn.length; i++) {
+  closeBtn[i].addEventListener("click", () => {
+    mobilOpen.classList.remove("active");
+    Body.classList.remove("active");
+    listTwo.classList.remove("active");
   });
+}
+
+let listTwo = document.querySelector(".list-two");
+let openSub = document.querySelector(".open-sub");
+let geriBtn = document.querySelector(".geri");
+
+openSub.addEventListener("click", () => {
+  listTwo.classList.add("active");
+});
+
+geriBtn.addEventListener("click", () => {
+  listTwo.classList.remove("active");
+});
